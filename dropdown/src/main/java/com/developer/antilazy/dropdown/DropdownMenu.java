f0355d7e0a4b700fade6faa1ea87c29d43d51201
gulrimkang <slldev92@gmail.com>
2019-03-15 13:00:19 +0900

    Add missing resource file

--- dropdown/src/main/java/com/developer/antilazy/dropdown/DropdownMenu.java
+++ dropdown/src/main/java/com/developer/antilazy/dropdown/DropdownMenu.java
@@ -11,7 +11,6 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.developer.antilazy.menus.R;
 
 
 /* This Module uses TextInputEditText, TextInputLayout, and customized spinner to implement
